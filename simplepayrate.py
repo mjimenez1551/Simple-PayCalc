119794b78fc93385bef7522355b2af8538f08aa6
Miguel Jimenez <Miguel.jimenez1551@gmail.com>
2016-12-09 21:34:08 -0600

    Update simplepayrate.py

--- simplepayrate.py
+++ simplepayrate.py
@@ -1,26 +1,22 @@
-"""Pay check calculator with single entry error checking with overtime
-using time and a half calculations used in python for everyone (coursera)"""
-hrs = raw_input("How Many hours were worked:\n") #Asks for user input
-try:
-    hrs=float(hrs) #Trys to convert user input to a floating number
-except:
-    print("You entered an invalid number") #Executes if a string was used instead of number
-    quit()
-payrate = raw_input("What is the payrate:\n") #Asks for user input
-try:
-    payrate=float(payrate) #Trys to convert user input to a floating number
-except:
-    print ("You entered an invalid number") #Executes if a string was used instead of number
-    quit()
-if hrs < 0 or payrate < 0 :
-    print "Hours and payrate can not be negative numbers"
-    quit ()
-elif hrs > 40: #Checks if more then 40 hours were worked
-        overtime=((hrs-40)*(payrate*1.5)) #Calculates and sets overtime pay
-        reghrs=(40*payrate) #Calculates and sets regular pay
-        totalpay=overtime+reghrs 
-else: #Executes if no overtime was worked
-    totalpay=hrs*payrate  #Calculates and sets total pay
-print totalpay #Prints total pay
-
+def computepay(hrs,rate):
+    try:
+        hrs=float(hrs)
+        rate=float(rate)
+        if hrs < 0 or rate < 0 :
+   			print "Hours and payrate can not be negative numbers"
+        elif hrs > 40:
+            overtime=((hrs-40)*(rate*1.5))
+            reghrs=(40*rate)
+            totalpay=overtime+reghrs
+            return totalpay
+        elif hrs <= 40 and hrs >= 1 :
+            totalpay=hrs*rate
+            return totalpay
+    except:
+        print "Invialid numbers entered"
+        quit()
+hrs = raw_input("Enter Hours:")
+rate = raw_input("Enter Payrate:")
+p = computepay(hrs,rate)
+print p
 """Coded By:Miguel Jimenez"""
